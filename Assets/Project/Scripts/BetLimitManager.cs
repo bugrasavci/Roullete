b8a373e113c8b85834166cda99bf62456fcfb14d
bugrasavci <saltuk.savci@gmail.com>
2025-04-06 23:28:43 +0300

    feat: Add Bet system for managing player bets

---
+++ Assets/Project/Scripts/BetLimitManager.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Project.Scripts;
+using TMPro;
+using UnityEngine;
+
+public class BetLimitManager : Singleton<BetLimitManager>
+{
+    [SerializeField] private float max = 1000f;
+    [SerializeField] private float min = 1f;
+
+    [SerializeField] private TMP_Text minT;
+    [SerializeField] private TMP_Text maxT;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        minT.text = $" Min: {min.ToString("0.0")}";
+        maxT.text = $" Min: {max.ToString("0.0")}";
+    }
+
+    public void SetBetLimits(float minBet, float maxBet)
+    {
+        min = minBet;
+        max = maxBet;
+    }
+
+    public bool AllowLimit(float value)
+    {
+        if (ResultManager.Instance.totalBet + value > max || ResultManager.Instance.totalBet + value < min)
+        {
+            return false;
+        }
+        return true;
+    }
+}
