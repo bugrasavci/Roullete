de800a7125fb02872629b4801f3d47d02300b622
bugrasavci <saltuk.savci@gmail.com>
2025-04-06 23:27:07 +0300

    feat: Add ToolTipManager to show tooltips on button clicks

---
+++ Assets/Project/Scripts/ToolTipManager.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Project.Scripts;
+using TMPro;
+using UnityEngine;
+
+public class ToolTipManager : Singleton<ToolTipManager>
+{
+    [SerializeField] private GameObject toolTip;
+    [SerializeField] private TMP_Text toolTipText;
+    private ChipStack Target;
+
+    public void SelectTarget(ChipStack stack)
+    {
+        Target = stack;
+
+        if (Target)
+        {
+            toolTip.SetActive(true);
+            toolTip.transform.position = Camera.main.WorldToScreenPoint(Target.transform.position);
+            toolTipText.text = Target.GetValue().ToString();
+        }
+    }
+    public void Deselect()
+    {
+        Target = null;
+        toolTip.SetActive(false);
+    }
+}
